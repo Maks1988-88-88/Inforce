bebe46a8ca5ce35ffc9c517fbbd970286908d619
Maksim Tsopa <maks98242@gmail.com>
2022-01-11 23:23:19 +0200

    add btn product

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,91 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { nanoid } from 'nanoid';
+import Modal from 'components/Modal/Modal';
 
 function App() {
+  const [modalState, setModalState] = useState(false);
+  const [products, setProduct] = useState([
+    {
+      id: 1,
+      name: 'Apple',
+      count: 3,
+      size: {
+        width: 200,
+        height: 200,
+      },
+      comments: ['CommentModel', 'CommentModel'],
+    },
+    {
+      id: 2,
+      name: 'Pear',
+      count: 2,
+      size: {
+        width: 200,
+        height: 200,
+      },
+      comments: ['CommentModel', 'CommentModel'],
+    },
+  ]);
+
+  useEffect(() => {
+    const productsStorage = JSON.parse(localStorage.getItem('products'));
+    if (productsStorage) {
+      setProduct(productsStorage);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(products));
+  }, [products]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    toggleModal();
+  };
+
+  const toggleModal = () => {
+    setModalState(!modalState);
+  };
+
+  const modalSubmit = data => {
+    if (products.some(product => product.name === data)) {
+      alert(`${data} is already in product.`);
+      return;
+    }
+    if (data.length < 1) {
+      alert(`error entered empty text`);
+      return;
+    }
+    const product = {
+      id: nanoid(10),
+      name: data,
+      count: 3,
+      size: {
+        width: 200,
+        height: 200,
+      },
+      comments: ['CommentModel', 'CommentModel'],
+    };
+    setProduct([product, ...products]);
+  };
+
   return (
     <div className="App">
-      <p>Test</p>
+      <form onSubmit={handleSubmit}>
+        <button>Add product</button>
+        <ul>
+          {products.map(el => (
+            <li key={el.id}>{el.name}</li>
+          ))}
+        </ul>
+      </form>
+      {modalState &&
+        ReactDOM.createPortal(
+          <Modal setModalState={setModalState} modalSubmit={modalSubmit} />,
+          document.getElementById('portal'),
+        )}
     </div>
   );
 }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,49 @@
+import s from './Modal.module.css';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+const Modal = ({ setModalState, modalSubmit }) => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setModalState(false);
+    modalSubmit(name);
+    console.log(name);
+  };
+
+  const handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <form className={s.modal} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <button>Add</button>
+        <button type="button" onClick={() => setModalState(false)}>
+          Close
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
+
+Modal.propTypes = {
+  setModalState: PropTypes.func.isRequired,
+};
